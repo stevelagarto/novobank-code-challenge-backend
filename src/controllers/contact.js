f25c210be226f4d064161ebc8a605ca3201ae2fa
stevelagarto <stevenzgz@gmail.com>
2020-07-21 12:13:31 +0200

    chore: added cors and unique email check

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -13,13 +13,21 @@ exports.findAll = async (req, res) => {
 
 exports.createContact = async (req, res) => {
   try {
+    const findEmail = await Contact.findOne({ where: { email: req.body.email } })
+
+    if (req.body.first_name === '' || req.body.last_name === '' || req.body.email === '' || req.body.phone === '') throw new Error('All fields must have a value')
+
+    if (findEmail !== null) {
+      res.status(400)
+      throw new Error('Email already exists')
+    }
     const createdContact = await Contact.create(req.body)
     res.status(201)
-    return res.json(
-      createdContact
-    )
+
+    return res.json(createdContact)
   } catch (error) {
     res.status(500)
+
     return res.json({ error: error.message })
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
 
 const router = require('./router')
+
+app.use(cors())
 
 app.use(bodyParser.json())
 
